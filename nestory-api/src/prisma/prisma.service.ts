4f1a97833e43de9372c5311f80bcd5d81ec66989
dex <dwwwtan@gmail.com>
2026-04-17 23:42:38 +0700

    refactor(PrismaService): remove unused Pool import and simplify adapter initialization (v7 no need Pool)

--- nestory-api/src/prisma/prisma.service.ts
+++ nestory-api/src/prisma/prisma.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
@@ -14,13 +13,8 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
       throw new Error('DATABASE_URL environment variable is not defined');
     }
 
-    // 1 Init Connection Pool from pg
-    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg({ connectionString });
 
-    // 2. Pass the newly created instance pool to the PrismaPg adapter
-    const adapter = new PrismaPg(pool);
-
-    // 3. Pass the adapter to the PrismaClient constructor
     super({ adapter });
   }
 
